35dee277d73d7c8594956102a96a4609efcf1891
gespi12 <gespi12@lsu.edu>
2025-09-15 14:03:10 -0600

    commit

--- sketch.js
+++ sketch.js
@@ -1,15 +1,22 @@
-let circleX, circleY;
+let circleX =[]; 
+let circleY =[];
+let numCircles = 200;
 
 function setup() {
     createCanvas(screen.width, screen.height);
-    circleX =  random(width);
-    circleY =  random(height);
+    for (let i = 0; i < numCircles; i++) {
+        circleX[i] = random(width);
+        circleY[i] = random(height);
+    }
+
 }
 
 function draw() {
     background(200);
-    circleX += (mouseX - circleX) * 0.05;
-    circleY += (mouseY - circleY) * 0.05;
+    for (let i =0; i < numCircles; i++) {
+    circleX[i] += (mouseX - circleX[i]) * 0.005;
+    circleY[i] += (mouseY - circleY[i]) * 0.005;
     fill(255);
-    ellipse(circleX, circleY, 50, 50);
+    ellipse(circleX[i], circleY[i], 10, 10);
+}
 }
